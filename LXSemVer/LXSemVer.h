7d6465f73a8568796ff3bdbaeb4bc5289d67ebf4
Stan Chang Khin Boon <khinboon@gmail.com>
2015-10-22 04:09:16 +0800

    Create Mac OS X and iOS targets.

--- LXSemVer/LXSemVer.h
+++ LXSemVer/LXSemVer.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015 lxcid. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for LXSemVer.
 FOUNDATION_EXPORT double LXSemVerVersionNumber;
